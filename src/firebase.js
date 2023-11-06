9826010f198725ae5ce8241f3c0808d9b36e1045
Munthasir-10 <munthasir1010@gmail.com>
2023-11-06 19:55:55 +0530

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3EA0nkjb6bEVgipSjaa4SNdkew1nsunk",
+  authDomain: "gpt-3-1b309.firebaseapp.com",
+  projectId: "gpt-3-1b309",
+  storageBucket: "gpt-3-1b309.appspot.com",
+  messagingSenderId: "896823627295",
+  appId: "1:896823627295:web:af712e92a80cfa490a8f2a"
+};
+
+
+const app = initializeApp(firebaseConfig);
